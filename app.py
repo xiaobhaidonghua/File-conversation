c55d3a7ad60795afbb9fcf47992c078322ace6a7
xiaobhaidonghua <a23568831@gmail.com>
2025-02-08 01:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ def home():
         background: antiquewhite;
     }
 </style>"""
+from flask import Flask
+app = Flask(name)
+
+@app.route('/')
+def hello_world():
+    return 'GreyMatters'
+
+
+if name == "main":
+    app.run()
